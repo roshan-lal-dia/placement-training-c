126d903465b77e92e1add16ad036350eca0edeef
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-30 06:25:36 +0000

    findihed v1

--- train-berth.c
+++ train-berth.c
@@ -8,28 +8,28 @@ int main() {
     switch (mod)
     {
     case 1 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'L1');
         break;
     case 2 :
-        printf("Seat pos is: %c", pos = 'lower2');
+        printf("Seat pos is: %c", pos = 'M1');
         break;        
     case 3 :
-        printf("Seat pos is: %c", pos = 'lower3');
+        printf("Seat pos is: %c", pos = 'U1');
         break;  
     case 4 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'L2');
         break;          
     case 5 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'M2');
         break;
     case 6 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'U2');
         break;   
     case 7 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'LW');
         break;       
     case 0 :
-        printf("Seat pos is: %c", pos = 'lower1');
+        printf("Seat pos is: %c", pos = 'UP');
         break;              
     }
     
