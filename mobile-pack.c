8280667ad972d3ae308d9ca2c8a7b42faf790c22
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-30 08:50:34 +0000

    on the input flow

--- mobile-pack.c
+++ mobile-pack.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main () {
-    int packChoice;
+    int packChoice, day, calls, sms, data;
 
     printf("Enter your mobile pack:");
     printf("\n1)199 \n2)299 \n3)799");
     printf("\nSelect a pack:");
     scanf("%d", &packChoice);
-    printf("%d", packChoice);
+    //printf("%d", packChoice);
+
+    printf("You have selcted the pack: %d \nPlease enter further details:", packChoice,);
+
+   printf("What is the day:");
+   scanf
+
+    switch (packChoice)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        printf("Your selected pack is invalid");
+        break;
+    }
 
     return 0;
 }
