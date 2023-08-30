b8657bf0defc98070ca8fbf1e4b42abc8f95ad70
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-30 05:18:14 +0000

    started

---
+++ grade-if-else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char grade = "";
+    int marks = 0;
+
+    if (marks <=100 || >=90){
+        grade = "S";
+        printf("Grade : %c", grade);
+    }else if (marks <=90 || >=80)
+    {
+        grade = "S";
+        printf("Grade : %c", grade);
+
+    }else if (marks <=90 || >=80)
+    {
+        grade = "S";
+        printf("Grade : %c", grade);
+
+    }
+    
+}
