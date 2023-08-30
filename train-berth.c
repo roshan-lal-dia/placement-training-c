53c7b32099443afbabb55baf928840d23e505bd0
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-30 06:32:59 +0000

    ambigously finished

--- train-berth.c
+++ train-berth.c
@@ -1,35 +1,35 @@
-#include <stdio.h>>
+#include <stdio.h>
 
 int main() {
-    char pos;
-    int berth = 8;
+    int berth = 57;
     int mod = berth % 8;
+   // printf("mod is: %d", mod);
 
     switch (mod)
     {
     case 1 :
-        printf("Seat pos is: %c", pos = 'L1');
+        printf("Seat pos is: L1");
         break;
     case 2 :
-        printf("Seat pos is: %c", pos = 'M1');
+        printf("Seat pos is: M1");
         break;        
     case 3 :
-        printf("Seat pos is: %c", pos = 'U1');
+        printf("Seat pos is: U1");
         break;  
     case 4 :
-        printf("Seat pos is: %c", pos = 'L2');
+        printf("Seat pos is: L2");
         break;          
     case 5 :
-        printf("Seat pos is: %c", pos = 'M2');
+        printf("Seat pos is: M2");
         break;
     case 6 :
-        printf("Seat pos is: %c", pos = 'U2');
+        printf("Seat pos is: U2");
         break;   
     case 7 :
-        printf("Seat pos is: %c", pos = 'LW');
+        printf("Seat pos is: LW");
         break;       
     case 0 :
-        printf("Seat pos is: %c", pos = 'UP');
+        printf("Seat pos is: UW");
         break;              
     }
     
