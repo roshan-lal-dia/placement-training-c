d4a7f20fd6ffd02005ba24b730891c811c24c4a5
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-31 08:46:11 +0000

    code logic done --brain not working --fix sign

--- .vscode/OddEvenDigitSum.c
+++
@@ -1,7 +0,0 @@
-//In given num, separate odd and even  then find the difference.
-#include <stdio.h>
-
-int main () {
-    
-    return 0;
-}
---
+++ OddEvenDigitSum.c
@@ -0,0 +1,29 @@
+//In given num, separate odd and even  then find the difference.
+#include <stdio.h>
+
+int main () {
+    int oddSum = 0, evenSum = 0, number = 0, rem = 0;
+
+    scanf("%d", &number);
+
+    while (number > 0)
+    {
+        rem = number % 10;
+        number /=10;
+
+    if (rem %2 == 0 )
+    {
+        evenSum += rem;
+    }else{
+        oddSum += rem;
+    }
+    }
+    
+    int diff = -oddSum - evenSum;
+
+    //diff *= (-1); @not working
+
+    printf("Diff: %d", diff );
+
+    return 0;
+}
