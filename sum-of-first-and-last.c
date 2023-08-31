43cf144d41469621e74b5cab04624bf5e1c8163f
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-31 04:06:54 +0000

    finished dynamiic divider

--- sum-of-first-and-last.c
+++ sum-of-first-and-last.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int num, first, last, digit, sum = 0;
+    int num, first = num, last,sum = 0; //first = num, for dynamic inding
 
     printf("Enter an integer: ");
     scanf("%d", &num);
 
-    first = num / 100; //first num of three digit num
+   // first = num / 100; //first num of three digit num
     
     //@find dynamically
-    /*while (first > 100)
+    while (first > 10)
     {
         first /=10;
-    }*/
+
+    }
     
 
     // Calculate the last digit
