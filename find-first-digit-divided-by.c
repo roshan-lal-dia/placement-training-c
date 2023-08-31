ab0ca232238c75f57fa77358a59c0c1b72c394e7
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-31 04:31:08 +0000

    fucked up

--- find-first-digit-divided-by.c
+++ find-first-digit-divided-by.c
@@ -1,13 +1,21 @@
 // to find the value that is used to get the value divided by
 //to get the first number
+//fuucked up
 
 #include <stdio.h>
 
 int main() {
-    int inp = 0;
+    int num = 0;inp = 0, modValue = 1, count = 1;
 
     //@get input values
-    scanf("%d", @inp);
-    
+    scanf("%d", &inp);
+
+    // Calculate the count of digits
+    while (num > 0) {
+        num /= 10;
+        count ++;
+        modValue *= 10;
+    }
+    printf("%d", modValue);
     return 0;
 }
