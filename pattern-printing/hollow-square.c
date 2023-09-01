7c531a0c9fee4385d33d4bcd7b8ca2faa29a5786
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 04:38:28 +0000

    printing working --time to logic

--- pattern-printing/hollow-square.c
+++ pattern-printing/hollow-square.c
@@ -11,5 +11,15 @@ int main () {
     int input = 0;
     scanf("%d", &input);
 
+    for (int i = 1; i <= input; i++)
+    {
+        for (int j = 1; j <= input; j++)
+        {
+            printf("* ");
+        }printf("\n");
+        
+    }
+    
+
     return 0;
 }
