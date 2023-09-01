7e213ff378c9bd99af5a76318a06b68d18df4669
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 05:09:32 +0000

    finished x

--- pattern-printing/x-pattern.c
+++ pattern-printing/x-pattern.c
@@ -8,30 +8,20 @@
 
 #include <stdio.h>
 int main () {
-    int input = 0;
+    int input;
     scanf("%d", &input);
 
     for (int i = 1; i <= input; i++)
     {
-        for (int j = 1; j <= input; j++)
+        for (int j = 1; j <=input; j++)
         {
             
-            /*if (i == 1 || i == input)
-            {
-                printf("* ");
-            }else if (j == 1 || j == input)
-            {
-                printf("* ");
-            }else{
-                printf("  "); //2 pixels
-            }*/
-
             //@less code
-            if (i == 1 || i == input || j == 1 || j == input)
+            if (i == j || i+j == input+1)// input + 1 for sum input = 5 . sum = sum +1= 6
             {
-                printf("* ");
+                printf("*");
             }else{
-                printf("  ");
+                printf(" ");
             }
             
             
