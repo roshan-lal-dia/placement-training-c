b3bc6bc4cd0d34e7fa629d98591e24d5c37c4fef
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 06:34:03 +0000

    array odd even completed --moving to sum

--- array/OddEvenDigitSum.c
+++ array/OddEvenDigitSum.c
@@ -8,12 +8,17 @@ int main () {
 
     for (int i = 0; i < ArraySize; i++)
     {
-        scanf("%d", &Number[i]);
+        scanf("%d", &Number[i]);   
     }
     
     for (int i = 0; i < ArraySize; i++)
     {
-        printf("%d", Number[i]);
+        if(Number[i] % 2 ==0 )
+        {
+            printf("\n%d --Even", Number[i]);
+        }else{
+            printf("\n%d --Odd", Number[i]);
+        }     
     }
 
     return 0;
