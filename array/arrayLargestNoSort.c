3e5b1217610314c6ce2340bbf22ab35a4a0bb4c9
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 09:09:23 +0000

    added code completed

--- array/arrayLargestNoSort.c
+++ array/arrayLargestNoSort.c
@@ -4,15 +4,20 @@ int main()
     int a;
     scanf("%d",&a);
     int ar[a];
+    int Large = 0;
     for(int i=0;i<a;i++)
     {
         scanf("%d",&ar[i]);
     }
 
-    /*for(int i=0;i<a;i++)
+    for(int i=0;i<a;i++)
     {
+      if (Large < ar[i])
+      {
+         Large = ar[i];
+      }
+      
+    }
 
-    }*/
-
-      printf("%d ",ar[ a  ]); //if < then, ar[1]
+      printf("%d ",Large);
 }
