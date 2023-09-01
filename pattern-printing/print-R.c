a4a8870da62d9e653b1c1ceb7b6300d63d4a81df
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 05:48:26 +0000

    added r file

---
+++ pattern-printing/print-R.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main () {
+    int input;
+    scanf("%d", &input);
+
+    for (int i = 1; i <= input; i++)
+    {
+        for (int j = 1; j <=input; j++)
+        {
+            
+            //@less code
+            if ()
+            {
+                printf("*");
+            }else{
+                printf(" ");
+            }
+            
+            
+        }printf("\n");
+        
+    }
+    
+
+    return 0;
+}
