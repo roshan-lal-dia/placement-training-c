052c15521862b60931ab868513ca3a76a3c8e435
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 04:52:55 +0000

    added pATTERN VISUALIZATION

---
+++ pattern-printing/x-pattern.c
@@ -0,0 +1,44 @@
+/*
+    *   *
+     * *
+      *
+     * *
+    *   *
+*/
+
+#include <stdio.h>
+int main () {
+    int input = 0;
+    scanf("%d", &input);
+
+    for (int i = 1; i <= input; i++)
+    {
+        for (int j = 1; j <= input; j++)
+        {
+            
+            /*if (i == 1 || i == input)
+            {
+                printf("* ");
+            }else if (j == 1 || j == input)
+            {
+                printf("* ");
+            }else{
+                printf("  "); //2 pixels
+            }*/
+
+            //@less code
+            if (i == 1 || i == input || j == 1 || j == input)
+            {
+                printf("* ");
+            }else{
+                printf("  ");
+            }
+            
+            
+        }printf("\n");
+        
+    }
+    
+
+    return 0;
+}
