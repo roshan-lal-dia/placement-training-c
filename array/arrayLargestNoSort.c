285c32ffbd4783937f28f86727cc462cd918fc2f
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-09-01 09:04:45 +0000

    fixed codes

--- array/arrayLargestNoSort.c
+++ array/arrayLargestNoSort.c
@@ -8,18 +8,11 @@ int main()
     {
         scanf("%d",&ar[i]);
     }
-    for(int i=0;i<a;i++)
-    {
-        for(int j=i+1;j<a;j++)
-        {
-            if(ar[i]>ar[j]) //change to < to store in descending order
-            {
-                int temp=ar[i];
-                ar[i]=ar[j];
-                ar[j]=temp;
-            }
-        }
-    }
 
-      printf("%d ",ar[ a -2 ]); //if < then, ar[1]
+    /*for(int i=0;i<a;i++)
+    {
+
+    }*/
+
+      printf("%d ",ar[ a  ]); //if < then, ar[1]
 }
