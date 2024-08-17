14b4cbebddae343e367b2f45411e21a12608fd9d
Guru Rahul <100460204+AntleWheels@users.noreply.github.com>
2024-08-18 00:37:57 +0530

    Update neon-number.c

--- neon-number.c
+++ neon-number.c
@@ -1,1 +1,28 @@
-//add the code
+Here is the program to check weather the number is a Neon number 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number, squared_value, count = 0;
+    
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    
+    squared_value = number * number;
+    
+    char squared_str[20];
+    sprintf(squared_str, "%d", squared_value);
+    
+    for (int i = 0; squared_str[i] != '\0'; i++) {
+        count += squared_str[i] - '0';  
+    }
+    
+    if (count == number) {
+        printf("%d is a Neon number\n", number);
+    } else {
+        printf("%d is not a Neon number\n", number);
+    }
+
+    return main();
+}
+
